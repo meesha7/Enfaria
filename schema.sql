7a3d0449a347512bf8d95076911bf4f8210c09c3
Meesha <44530786+meesha7@users.noreply.github.com>
2020-12-19 16:31:35 +0100

    Change encoding to utf8mb4

--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `users` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`email` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`username` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`username` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`password` BINARY(60) NOT NULL,
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='utf8_general_ci'
+COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
 
 CREATE TABLE `sessions` (
 	`user_id` INT(11) NOT NULL,
-	`secret` VARCHAR(300) NOT NULL COLLATE 'utf8_general_ci',
+	`secret` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`expiry_date` DATETIME NOT NULL,
 	PRIMARY KEY (`user_id`) USING BTREE
 )
-COLLATE='utf8_general_ci'
+COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
