c2c751b2338ac76c65d8daf5abfdeaf1d43808c2
Meesha <44530786+meesha7@users.noreply.github.com>
2020-12-19 16:31:17 +0100

    Use binary column for password hash

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE `users` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
 	`username` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
-	`password` VARCHAR(300) NOT NULL COLLATE 'utf8_general_ci',
+	`password` BINARY(60) NOT NULL,
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8_general_ci'
