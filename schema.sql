254cffcaadbb2e988bbe6f6f27d3a01339d5f9e0
Meesha <44530786+meesha7@users.noreply.github.com>
2020-12-19 16:41:26 +0100

    Add foreign key to session user ID

--- schema.sql
+++ schema.sql
@@ -13,7 +13,11 @@ CREATE TABLE `sessions` (
 	`user_id` INT(11) NOT NULL,
 	`secret` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`expiry_date` DATETIME NOT NULL,
-	PRIMARY KEY (`user_id`) USING BTREE
+	PRIMARY KEY (`user_id`) USING BTREE,
+    CONSTRAINT `fk_session_user`
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
