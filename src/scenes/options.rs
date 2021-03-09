873094ee0401aab766074e661b8474f4b7490f4a
Meesha <44530786+meesha7@users.noreply.github.com>
2021-03-09 13:39:41 +0100

    Config serialization

--- src/scenes/options.rs
+++ src/scenes/options.rs
@@ -2,32 +2,68 @@ use crate::scenes::{Scene, SceneSwitch};
 use crate::world::GameWorld;
 use egui::*;
 use log::info;
+use serde::ser::{Serialize, Serializer};
+use serde_derive::Serialize as SerializeMacro;
 use tetra::{Context, Event};
 
-#[derive(Debug, PartialEq)]
+// Resolution options
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Resolution {
     Res1080,
     Res768,
     Res600,
 }
 
-#[derive(Debug, Default)]
-pub struct OptionsScene {
-    back_clicked: bool,
-    resolution_selected: Resolution,
-    fullscreen: bool
-}
-
+// Choose default resolution unless it's provided by config
 impl Default for Resolution {
     fn default() -> Self {
         Resolution::Res1080
     }
 }
 
+// Serializing Resolution into the config file
+impl Serialize for Resolution {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            Resolution::Res1080 => serializer.serialize_unit_variant("resolution", 0, "1920x1080"),
+            Resolution::Res768 => serializer.serialize_unit_variant("resolution", 1, "1024x768"),
+            Resolution::Res600 => serializer.serialize_unit_variant("resolution", 2, "800x600"),
+        }
+    }
+}
+
+// Our options scene
+#[derive(Debug, Default)]
+pub struct OptionsScene {
+    back_clicked: bool,
+    resolution_selected: Resolution,
+    fullscreen: bool,
+}
+
 impl OptionsScene {
     pub fn new(_world: &mut GameWorld, _ctx: &mut Context) -> Self {
         OptionsScene::default()
     }
+}
+
+// Config struct to pass scene options into the config saving function
+#[derive(Debug, SerializeMacro)]
+struct Config {
+    resolution: Resolution,
+    fullscreen: bool,
+}
+
+// Saves config into file
+fn save_config(config: Config) {
+    info!("Saving config");
+
+    let toml = toml::to_string(&config).unwrap();
+    info!("{:?}", toml);
+
+    // TODO: Implement saving
 }
 
 impl Scene for OptionsScene {
@@ -63,6 +99,13 @@ impl Scene for OptionsScene {
 
                     if back.clicked() {
                         info!("Clicked back");
+
+                        let config = Config {
+                            resolution: self.resolution_selected,
+                            fullscreen: self.fullscreen,
+                        };
+
+                        save_config(config);
                         self.back_clicked = true;
                     }
                 });
